ed1123bbdc7e792952e59d48fe1e028e40ab63f3
Mx <1318892153@qq.com>
2022-05-12 19:58:59 +0800

    克里斯·保罗

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 xxx.onload = function () {
-            console.log('图片加载成功');
-        }
-        xxx.onerror = function () {
-            console.log('图片加载失败');
-        }
-        
+  console.log("图片加载成功");
+};
+xxx.onerror = function () {
+  console.log("图片加载失败");
+  xxx.src = "/404.png";
+};
